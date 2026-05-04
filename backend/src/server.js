021ea48b462913db70f561d3fb6f80e06242547d
adriyan <adrianadrian.aa613@gmail.com>
2026-05-04 18:11:11 +0700

    backend initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
